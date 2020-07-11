84ae9d9938a2469343a3ba2b995fd6478c9d28bd
tttyyy1456 <53494222+tttyyy1456@users.noreply.github.com>
2020-07-11 17:18:18 +0400

    Add files via upload

---
+++ build/precache-manifest.9e2d749adb6db06ed21968a4cb960e91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5869c31d92a1cb86620ed028757b193f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "48021c922b0af5884faa",
+    "url": "./static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8c1f37ec45e9757e7408",
+    "url": "./static/css/main.b0f860d7.chunk.css"
+  },
+  {
+    "revision": "48021c922b0af5884faa",
+    "url": "./static/js/2.e92e4bbd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e92e4bbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c1f37ec45e9757e7408",
+    "url": "./static/js/main.5cc70967.chunk.js"
+  },
+  {
+    "revision": "53b3f76c3c07d57afb17",
+    "url": "./static/js/runtime-main.25d35d46.js"
+  }
+]);
